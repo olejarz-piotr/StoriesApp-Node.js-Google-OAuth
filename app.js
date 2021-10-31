9a3b9a45ff93bcb19024ba6b5fc307cdca1ae0bf
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-31 16:55:38 +0100

    adding oauth account, modyfing views

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const dotenv= require('dotenv')
 const morgan = require('morgan');
 const exphbs=require('express-handlebars');
 const connectDB=require('./config/db')
+const passport = require('passport');
+const session=require('express-session');
+const path=require('path')
+
 
 dotenv.config({path: './config/config.env'})
+
+require('./config/passport')(passport)
 
 connectDB();
 const app=express();
@@ -15,9 +21,24 @@ if(process.env.MODE_ENV==='development'){
 app.engine('.hbs', exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine', '.hbs')
 
+//sessions
+app.use(session({
+    secret:'keyboard car',
+    resave:false,
+    saveUninitialized:false
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+
 //routes
 
 app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
 
 const PORT=process.env.PORT  || 5000
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', { layout: 'login' })
 })
 
 router.get('/dashboard', (req, res) => {
