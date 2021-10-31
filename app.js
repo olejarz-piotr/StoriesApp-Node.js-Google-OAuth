0940c95226c3d9ca02455bb1dadb850285f4de9e
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-31 14:09:51 +0100

    adding css stylesheets, creating basic views

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv= require('dotenv')
+const morgan = require('morgan');
+const exphbs=require('express-handlebars');
 const connectDB=require('./config/db')
 
 dotenv.config({path: './config/config.env'})
 
 connectDB();
 const app=express();
+if(process.env.MODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main',extname:'.hbs'}))
+app.set('view engine', '.hbs')
+
+//routes
+
+app.use('/',require('./routes/index'))
 
 const PORT=process.env.PORT  || 5000
 
